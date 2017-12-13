30fa5e8ef25f8fb3ffc664ea1905c5e76f1c1667
Rabbit <venkatasivaprasadreddypulagam@gmail.com>
2017-12-13 16:32:37 +0530

    Committing changes

--- Samples/MapsExample/MapsSample/MapsSample/Views/AccessingExternalMaps/ExternalMapsCallPage.xaml.cs
+++ Samples/MapsExample/MapsSample/MapsSample/Views/AccessingExternalMaps/ExternalMapsCallPage.xaml.cs
@@ -8,8 +8,8 @@ namespace MapsSample
 {
     public partial class ExternalMapsCallPage : ContentPage
     {
-        string strSelectedUrlScheme = "";
-        Dictionary<string, string> dictionaryUrlScheme;
+        string strSelectedUrlScheme = "", strSelectedMapType = "", strSelectedTransportType = "";
+        Dictionary<string, string> dictionaryUrlScheme, dictionaryMapType, dictionaryTransportType;
 
         public ExternalMapsCallPage()
         {
@@ -19,17 +19,50 @@ namespace MapsSample
             dictionaryUrlScheme = new Dictionary<string, string>()
             {
                 {"Route_Lat-Long_Lat-Long", string.Format("http://maps.google.com/?saddr=" + originLatitude + "," + originLongitude + "&daddr=" + destinationLatitude + "," + destinationLongitude + "")},
-                {"SearchLocation", string.Format("http://maps.google.com/?q=" + destinationLatitude + "," + destinationLongitude + "")}
+                {"Route from Current location to Lat-Long", string.Format("http://maps.google.com/?daddr=" + destinationLatitude + "," + destinationLongitude + "")},
+                {"Display Location at center using Lat_Long", string.Format("http://maps.google.com/?ll=" + destinationLatitude + "," + destinationLongitude + "")},
+                {"Display Location at center using Address", string.Format("http://maps.google.com/?address=" + "Charminar,+Hyderabad")},
+                {"SearchLocation", string.Format("http://maps.google.com/?q=" + destinationLatitude + "," + destinationLongitude + "")},
+                {"Search a thing at this Location", string.Format("http://maps.google.com/?q=" + "Stores"+ "&sll=" + destinationLatitude + "," + destinationLongitude + "")}
             };
-            List<string> listPickerItems = new List<string>();
+            List<string> listURLPickerItems = new List<string>();
             foreach (var items in dictionaryUrlScheme)
             {
-                listPickerItems.Add(items.Key);
+                listURLPickerItems.Add(items.Key);
             }
-            urlSchemePicker.ItemsSource = listPickerItems;
+            urlSchemePicker.ItemsSource = listURLPickerItems;
+
+
+            dictionaryMapType = new Dictionary<string, string>()
+            {
+                {"standard", "&t=m"},
+                {"satellite", "&t=k"},
+                {"hybrid", "&t=h"},
+                {"transit", "&t=r"}
+            };
+            List<string> listMapTypePickerItems = new List<string>();
+            foreach (var items in dictionaryMapType)
+            {
+                listMapTypePickerItems.Add(items.Key);
+            }
+            pickerMapType.ItemsSource = listMapTypePickerItems;
+
+
+            dictionaryTransportType = new Dictionary<string, string>()
+            {
+                {"car", "&dirflg=d"},
+                {"foot", "&dirflg=w"},
+                {"public transit", "&dirflg=r"}
+            };
+            List<string> listTransportTypePickerItems = new List<string>();
+            foreach (var items in dictionaryTransportType)
+            {
+                listTransportTypePickerItems.Add(items.Key);
+            }
+            pickerTransportType.ItemsSource = listTransportTypePickerItems;
         }
 
-        void PickerItemSelected(object sender, EventArgs e)
+        void URLSchemeItemSelected(object sender, EventArgs e)
         {
             try
             {
@@ -44,6 +77,46 @@ namespace MapsSample
                     strSelectedUrlScheme = dictionaryUrlScheme[owner.SelectedItem.ToString()];
                     //var selectedKey = dictionaryUrlScheme.Keys.Where(X => X == owner.SelectedItem.ToString()).ToString();
                     //int IndexofSelectedValue = dictionaryUrlScheme.Keys.ToList().IndexOf(owner.SelectedItem.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message;
+            }
+        }
+
+        void MapTypeItemSelected(object sender, EventArgs e)
+        {
+            try
+            {
+                var owner = ((Picker)sender);
+                if (owner.SelectedIndex <= -1)
+                {
+                    return;
+                }
+                else
+                {
+                    strSelectedMapType = dictionaryMapType[owner.SelectedItem.ToString()];
+                }
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message;
+            }
+        }
+
+        void TransportTypeItemSelected(object sender, EventArgs e)
+        {
+            try
+            {
+                var owner = ((Picker)sender);
+                if (owner.SelectedIndex <= -1)
+                {
+                    return;
+                }
+                else
+                {
+                    strSelectedTransportType = dictionaryTransportType[owner.SelectedItem.ToString()];
                 }
             }
             catch (Exception ex)
@@ -75,8 +148,9 @@ namespace MapsSample
                         mapUrl = string.Format("http://maps.google.com/?daddr=" + 17.4286 + "," + 78.4331 + "");
                         break;
                 }
+                var validUrl = strSelectedUrlScheme + strSelectedTransportType + strSelectedMapType;
                 //Device.OpenUri(new Uri(mapUrl));
-                Device.OpenUri(new Uri(strSelectedUrlScheme));
+                Device.OpenUri(new Uri(validUrl));
             }
             catch (Exception ex)
             {
