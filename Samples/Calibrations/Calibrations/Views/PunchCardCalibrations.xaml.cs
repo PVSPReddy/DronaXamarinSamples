5227702cc076311b5230070fa916ceb718039b91
Rabbit <venkatasivaprasadreddypulagam@gmail.com>
2018-01-18 18:44:12 +0530

    Committing changes

---
+++ Samples/Calibrations/Calibrations/Views/PunchCardCalibrations.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Calibrations.Views
+{
+    public partial class PunchCardCalibrations : ContentPage
+    {
+        public PunchCardCalibrations()
+        {
+            InitializeComponent();
+        }
+
+        void BackButtonTapped(object sender, EventArgs e)
+        {
+            try
+            {
+                Navigation.PopModalAsync();
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message;
+            }
+        }
+
+        void SubmitClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                labelDataDisplay.Text = "";
+                for (int i = 0; i < 100; i++ )
+                {
+                    var rows = (int)Math.Sqrt(i);
+                    var columns = (i / rows);
+                    var totalCellsFormed = rows * columns;
+                    labelDataDisplay.Text +=  "\n\n i: " + i.ToString() +"Rows: " + rows.ToString() + "Columns: " + columns.ToString() + "TotalCellsFormed: " + totalCellsFormed.ToString();
+                } 
+
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message;
+            }
+        }
+    }
+}
