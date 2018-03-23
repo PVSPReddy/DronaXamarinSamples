c5fc514069f1b7b3d92e1f1fecc22010f3c70dc1
Rabbit <venkatasivaprasadreddypulagam@gmail.com>
2018-03-23 18:29:45 +0530

    Committing changes

---
+++ Samples/CameraGallery/CameraGallery/CameraGalleryPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace CameraGallery
+{
+    public partial class CameraGalleryPage : ContentPage
+    {
+        public CameraGalleryPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
